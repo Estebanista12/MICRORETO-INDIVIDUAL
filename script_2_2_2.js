fc4c6b05bbbaca2ff702bda2623820b69be35551
Estebanista12 <emardom0111@g.educaand.es>
2026-02-26 09:19:46 +0100

    ultima actualizacion

---
+++ script_2_2_2.js
@@ -0,0 +1,8 @@
+// 1. Seleccionamos el título
+const titulo = document.querySelector("h1");
+
+// 2. Cambiamos su texto
+titulo.textContent = "¡DOM controlado por Esteban Martin!";
+
+// 3. Cambiamos su color de fondo
+titulo.style.backgroundColor = "beige";
